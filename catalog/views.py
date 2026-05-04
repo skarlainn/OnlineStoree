03d925382d55cb883e0075fc26cb975bd827a7c6
Diana <ddmitrieva098@example.com>
2026-05-04 21:57:46 +1100

    add views and urls

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Product
 
 def home(request):
     return render(request, template_name='home.html')
@@ -14,4 +15,15 @@ def contacts(request):
         return HttpResponse(f"Спасибо за обращение, {name}!")
 
     return render(request, template_name='contacts.html')
-# Create your views here.
+
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, template_name='product_list.html', context=context)
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {'product': product}
+    return render(request, template_name='product_details.html', context=context)
