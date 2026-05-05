19c8b6c5744112cdaf2d2ba35b89b855c6fa9e97
Diana <ddmitrieva098@example.com>
2026-05-05 20:16:08 +1100

    fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,6 +7,6 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('', products_list, name='products_list'),
+    path('product/', products_list, name='products_list'),
     path('product/<int:pk>/', product_details, name='product')
 ]
