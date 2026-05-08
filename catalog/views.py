90573abeeaf1a203fe41232da8c11fa0b96d86ff
Diana <ddmitrieva098@example.com>
2026-05-08 16:16:02 +1100

    change controllers from FBV to CBV in the catalog app

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Product
+from django.shortcuts import render
 
-def home(request):
-    return render(request, template_name='home.html')
+from .models import Product
+from django.views.generic import DetailView, ListView, TemplateView
+
+class HomeTemplateView(TemplateView):
+    template_name = 'catalog/home.html'
 
 
 def contacts(request):
@@ -14,16 +16,11 @@ def contacts(request):
 
         return HttpResponse(f"Спасибо за обращение, {name}!")
 
-    return render(request, template_name='contacts.html')
+    return render(request, template_name='catalog/home.html')
 
 
-def products_list(request):
-    products = Product.objects.all()
-    context = {'products': products}
-    return render(request, template_name='product_list.html', context=context)
+class ProductListView(ListView):
+    model = Product
 
-
-def product_details(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {'product': product}
-    return render(request, template_name='product_details.html', context=context)
+class ProductDetailView(DetailView):
+    model = Product
