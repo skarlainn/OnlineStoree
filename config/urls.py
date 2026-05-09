2880b07a25f134fc373d2687763be8a0368f490c
Diana <ddmitrieva098@example.com>
2026-05-09 21:24:42 +1100

    add controller

--- config/urls.py
+++ config/urls.py
@@ -7,4 +7,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
+    path('blogs/', include('blogs.urls', namespace='blogs'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
