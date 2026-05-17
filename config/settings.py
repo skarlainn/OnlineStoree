608309159e78767d4b1d620fe709423e59251d22
Diana <ddmitrieva098@example.com>
2026-05-17 14:33:09 +1100

    add forms

--- config/settings.py
+++ config/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 
-from django.conf.global_settings import AUTH_USER_MODEL
 from dotenv import load_dotenv
 from pathlib import Path
 
